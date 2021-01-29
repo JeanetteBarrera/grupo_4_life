22abcca6c3a05b55243e5e6c57786d0631fee46c
JeanetteBarrera <jeanette.barrera0710@gmail.com>
2021-01-29 13:48:53 -0300

    correcciones y agrego routes

--- MyProyectoLife/app.js
+++ MyProyectoLife/app.js
@@ -4,10 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+/*====  REQUERIMOS LOS ROUTERS ====*/
 var indexRouter = require('./routes/indexRouter');
+var productoRouter = require('./routes/productoRouter');
+var carritoRouter = require('./routes/carritoRouter');
 var usersRouter = require('./routes/users');
 
+
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +25,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+/* */
 app.use('/', indexRouter);
+app.use('/producto', productoRouter);
+app.use('/carrito', carritoRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
