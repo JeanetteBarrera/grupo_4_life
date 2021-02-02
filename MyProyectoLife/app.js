418790568cabb2c5b83271984b1ca8a985842967
BelenRocio <belenchiotta3@gmail.com>
2021-02-02 00:34:34 -0300

    Subo Formulario de Carga de producto (admin)

--- MyProyectoLife/app.js
+++ MyProyectoLife/app.js
@@ -9,6 +9,7 @@ var logger = require('morgan');
 var generalRouter = require('./routes/generalRouter');
 var usersRouter = require('./routes/usersRouter');
 var productsRouter = require('./routes/productsRouter');
+var adminRouter = require('./routes/adminRouter')
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', generalRouter);
 app.use('/account', usersRouter);
 app.use("/products", productsRouter);
+app.use("/admin", adminRouter);
 
 
 // catch 404 and forward to error handler
